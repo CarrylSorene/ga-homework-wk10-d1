5e38724c126a7a2c1a8b92672c4e6ac731f523d2
Carryl Sorene <carrylsorene@gmail.com>
2015-10-12 21:39:31 +0100

    Each cell displays X when any cell is clicked, glad to render something at this point

--- js/gameController.js
+++ js/gameController.js
@@ -4,29 +4,40 @@ angular.module('gameApp')
   function GameController(){
     //this.all = [];
 
+    this.playerMove = 'X';
+
     this.storeMoves = new Array(9);
 
     this.moveX = []
 
-    this.move0 = []
+    this.moveO = []
 
     this.newX = "X";
 
     this.addX = function() {
       console.log('working');
-      this.all.push(this.newX);
+      this.moveX.push(this.newX);
+    };
 
-    }
+    this.assignMove = function(num) {
+      console.log('working')
+      
+      if (this.playerMove === 'X') {
+        this.moveX.push(num)
+        console.log(num)
+        this.playerMove = "X"
+         //render X in td
+      };
 
+      if (this.playerMove === 'O'){
+        this.moveO.push(num)
+        console.log(num)
+        this.playerMove = "O"
+        //render O in td
+      };
+    };
   };
 
-// function MoveController() {
-
-// }
-
-// decide which of X or O starts
-
-// 9 cells is either x or o, or neither ""
-
+// 9 cells - either x or o, or neither ""
 // array of 9 all '', when you click that position in array with x or o
 // tell td to always display the value of array, then tell to 
