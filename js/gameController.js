8697db4cdf2839cc052373503915ac94d8bae99d
Carryl Sorene <carrylsorene@gmail.com>
2015-10-12 19:29:02 +0100

    Board displaying what it's told from the controller

--- js/gameController.js
+++ js/gameController.js
@@ -4,13 +4,26 @@ angular.module('gameApp')
   function GameController(){
     this.all = [];
 
+    this.storeMoves = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+    //new Array(9);
+
     this.newX = "X";
 
-    this.add = addX;
-
-    function addX() {
+    this.addX = function() {
+      console.log('working');
       this.all.push(this.newX);
-      console.log(this.newX);
-    };
+
+    }
 
   };
+
+// function MoveController() {
+
+// }
+
+// decide which of X or O starts
+// angular looks at a collection, when it changes, something can happen on page - so html needs to look at an array or object - 9 cells is either x or o, or neither ""
+
+// array of 9 all '', when you click that position in array with x or o
+// tell td to always display the value of array, then tell to 
+
