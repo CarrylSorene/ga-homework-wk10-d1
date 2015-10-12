506da6999ed74f60407308b904b1e9e93aff2534
Carryl Sorene <carrylsorene@gmail.com>
2015-10-12 20:11:04 +0100

    Defined starting variables for both types of move and removed hard-coding

--- js/gameController.js
+++ js/gameController.js
@@ -2,10 +2,13 @@ angular.module('gameApp')
   .controller('GameController', GameController);
 
   function GameController(){
-    this.all = [];
+    //this.all = [];
 
-    this.storeMoves = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-    //new Array(9);
+    this.storeMoves = new Array(9);
+
+    this.moveX = []
+
+    this.move0 = []
 
     this.newX = "X";
 
@@ -22,8 +25,8 @@ angular.module('gameApp')
 // }
 
 // decide which of X or O starts
-// angular looks at a collection, when it changes, something can happen on page - so html needs to look at an array or object - 9 cells is either x or o, or neither ""
+
+// 9 cells is either x or o, or neither ""
 
 // array of 9 all '', when you click that position in array with x or o
 // tell td to always display the value of array, then tell to 
-
